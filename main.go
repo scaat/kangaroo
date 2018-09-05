3e718856350e01f932ed0b051848145d2dbf98e1
Scaat Feng <scaat.feng@gmail.com>
2018-09-05 23:41:00 +0800

    feature: add IPC namespace function

--- main.go
+++ main.go
@@ -11,7 +11,7 @@ func main() {
 
 	cmd := exec.Command("sh")
 
-	cmd.SysProcAttr = &syscall.SysProcAttr{}
+	cmd.SysProcAttr = &syscall.SysProcAttr{Cloneflags: syscall.CLONE_NEWUTS | syscall.CLONE_NEWIPC}
 	cmd.Stdin = os.Stdin
 	cmd.Stdout = os.Stdout
 	cmd.Stderr = os.Stderr
