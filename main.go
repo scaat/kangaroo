1f77c2fb9ce9c479efa16bb9be4f88743e73adba
Scaat Feng <scaat.feng@gmail.com>
2018-09-06 00:00:11 +0800

    feature: add User namespace function

--- main.go
+++ main.go
@@ -15,7 +15,10 @@ func main() {
 												syscall.CLONE_NEWUTS |
 												syscall.CLONE_NEWIPC |
 												syscall.CLONE_NEWPID |
-												syscall.CLONE_NEWNS}
+												syscall.CLONE_NEWNS |
+												syscall.CLONE_NEWUSER}
+	cmd.SysProcAttr.Credential = &syscall.Credential{Uid: uint32(1), Gid: uint32(1)}
+
 	cmd.Stdin = os.Stdin
 	cmd.Stdout = os.Stdout
 	cmd.Stderr = os.Stderr
@@ -23,4 +26,6 @@ func main() {
 	if err := cmd.Run(); err != nil {
 		log.Fatal(err)
 	}
+
+	os.Exit(-1)
 }
