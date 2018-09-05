edf7daa7817f940fa174e5f056f6a8fa9c495e41
Scaat Feng <scaat.feng@gmail.com>
2018-09-06 00:12:02 +0800

    feature: add Network namespace function

--- main.go
+++ main.go
@@ -16,7 +16,8 @@ func main() {
 												syscall.CLONE_NEWIPC |
 												syscall.CLONE_NEWPID |
 												syscall.CLONE_NEWNS |
-												syscall.CLONE_NEWUSER}
+												syscall.CLONE_NEWUSER |
+												syscall.CLONE_NEWNET}
 	// cmd.SysProcAttr.Credential = &syscall.Credential{Uid: uint32(1), Gid: uint32(1)}
 	cmd.SysProcAttr.UidMappings = []syscall.SysProcIDMap{{ContainerID: 5001, HostID: syscall.Getuid(), Size: 1}}
 	cmd.SysProcAttr.GidMappings = []syscall.SysProcIDMap{{ContainerID: 5001, HostID: syscall.Getuid(), Size: 1}}
