345748ee273fc752919bc84697bdbe43e6497226
Scaat Feng <scaat.feng@gmail.com>
2018-09-10 22:48:13 +0800

    feature: add initCommand and runCommand function

---
+++ main_command.go
@@ -0,0 +1,53 @@
+package main
+
+import (
+	"github.com/urfave/cli"
+	"fmt"
+	"github.com/lexkong/log"
+	"github.com/scaat/kangaroo/container"
+)
+
+var runCommand = cli.Command{
+	Name: "run",
+	Usage: `Create a container with namespace and cgroups limit
+			kangaroo run -ti [command]`,
+	Flags: []cli.Flag{
+		cli.BoolFlag{
+			Name: "ti",
+			Usage: "enable tty",
+		},
+	},
+
+	/*
+	这里是run命令执行的真正函数
+	1. 判断参数是否包含command
+	2. 获取用户指定的command
+	3. 调用 Run function 去准备启动容器
+	 */
+	Action: func(context *cli.Context) error {
+		if len(context.Args()) < 1 {
+			return fmt.Errorf("missing container command")
+		}
+		cmd := context.Args().Get(0)
+		tty := context.Bool("ti")
+		Run(tty, cmd)
+		return nil
+	},
+}
+
+var initCommand = cli.Command{
+	Name: "init",
+	Usage: "Init container process run user's process in container. Do not call it outside",
+
+	/*
+	1. 获取传递过来的 command 参数
+	2. 执行容器初始化操作
+	 */
+	Action: func(context *cli.Context) error {
+		log.Info("init come on")
+		cmd := context.Args().Get(0)
+		log.Infof("command %s", cmd)
+		err := container.RunContainerInitProcess(cmd, nil)
+		return err
+	},
+}
