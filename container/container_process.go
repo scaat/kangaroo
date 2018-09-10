59b769784eaa700c9f4cc5a7bf26048b5174695c
Scaat Feng <scaat.feng@gmail.com>
2018-09-10 23:18:39 +0800

    bugfix: fix bug for '/proc/self/exec: no such file or directory'

--- container/container_process.go
+++ container/container_process.go
@@ -8,7 +8,7 @@ import (
 
 func NewParentProcess(tty bool, command string) *exec.Cmd {
 	args := []string{"init", command}
-	cmd := exec.Command("/proc/self/exec", args...)
+	cmd := exec.Command("/proc/self/exe", args...)
 	cmd.SysProcAttr = &syscall.SysProcAttr{
 		Cloneflags: syscall.CLONE_NEWUTS | syscall.CLONE_NEWPID | syscall.CLONE_NEWNS | syscall.CLONE_NEWNET | syscall.CLONE_NEWIPC,
 	}
